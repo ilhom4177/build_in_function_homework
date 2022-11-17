75c325b6266c55397b10021eb7e60ef7e5a1f7b2
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-17 17:01:03 +0500

    build_func02.py

--- build_func02.py
+++ build_func02.py
@@ -1,11 +1,11 @@
-def main(n):
+def main():
     """
     Calculate the value of expression and return result:
 
     Returns:
         result: float
     """
-    x=3*(7/5-9/4)
-    return x
-print(main(7))   
+    c=  (round(3*(7/5-9/4)))
+    return c
+print(main())   
     
